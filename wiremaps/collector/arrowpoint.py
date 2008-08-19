62f8a1a6b539dde8ab18964fb342c0f631fd4882
Vincent Bernat <vbernat@wanadooportails.com>
2008-08-19 16:06:54 +0200

    Support for Arrowpoint

---
+++ wiremaps/collector/arrowpoint.py
@@ -0,0 +1,29 @@
+from zope.interface import implements
+from twisted.plugin import IPlugin
+
+from wiremaps.collector.icollector import ICollector
+from wiremaps.collector.port import PortCollector
+from wiremaps.collector.arp import ArpCollector
+
+class ArrowPoint:
+    """Collector for Arrowpoint Content Switch (no FDB)"""
+
+    implements(ICollector, IPlugin)
+
+    def handleEquipment(self, oid):
+        return (oid in ['.1.3.6.1.4.1.2467.4.2', # CS-800
+                        '.1.3.6.1.4.1.2467.4.3', # CS-1100
+                        ])
+
+    def normPortName(self, port):
+        return "Port %s" % port
+
+    def collectData(self, ip, proxy, dbpool):
+        ports = PortCollector(proxy, dbpool, self.normPortName)
+        arp = ArpCollector(proxy, dbpool)
+        d = ports.collectData()
+        d.addCallback(lambda x: arp.collectData(write=False))
+        d.addCallback(lambda x: arp.collectData())
+        return d
+
+arrow = ArrowPoint()
