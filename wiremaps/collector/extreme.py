f9652507d3062a554b9844a2008d6b18d47c66de
Vincent Bernat <vbernat@wanadooportails.com>
2008-08-19 15:38:52 +0200

    Add Extreme Black Diamond 6808 support

--- wiremaps/collector/extreme.py
+++ wiremaps/collector/extreme.py
@@ -4,12 +4,12 @@ from twisted.internet import defer
 
 from wiremaps.collector.icollector import ICollector
 from wiremaps.collector.port import PortCollector
-from wiremaps.collector.fdb import FdbCollector
+from wiremaps.collector.fdb import FdbCollector, ExtremeFdbCollector
 from wiremaps.collector.arp import ArpCollector
 from wiremaps.collector.lldp import LldpCollector
 
 class ExtremeSummit:
-    """Collector for Extreme Summit and BlackDiamond switches"""
+    """Collector for Extreme Summit and BlackDiamond 8810 switches"""
 
     implements(ICollector, IPlugin)
 
@@ -18,7 +18,6 @@ class ExtremeSummit:
                         '.1.3.6.1.4.1.1916.2.54', # Extreme Summit 48e
                         '.1.3.6.1.4.1.1916.2.62', # Black Diamond 8810
                         '.1.3.6.1.4.1.1916.2.76', # Extreme Summit 48t
-                        '.1.3.6.1.4.1.1916.2.11', # Black Diamond 6808
                         ])
 
     def collectData(self, ip, proxy, dbpool):
@@ -35,4 +34,28 @@ class ExtremeSummit:
         d.addCallback(lambda x: arp.collectData())
         return d
 
+class ExtremeWare:
+    """Collector for BlackDiamond 6808"""
+
+    implements(ICollector, IPlugin)
+
+    def handleEquipment(self, oid):
+        return (oid in ['.1.3.6.1.4.1.1916.2.11', # Black Diamond 6808 (ExtremeWare)
+                        ])
+
+    def collectData(self, ip, proxy, dbpool):
+        ports = PortCollector(proxy, dbpool)
+        fdb = ExtremeFdbCollector(proxy, dbpool)
+        arp = ArpCollector(proxy, dbpool)
+        # LLDP disabled due to unstability
+        # lldp = LldpCollector(proxy, dbpool)
+        d = ports.collectData()
+        d.addCallback(lambda x: fdb.collectData(write=False))
+        d.addCallback(lambda x: arp.collectData(write=False))
+        # d.addCallback(lambda x: lldp.collectData())
+        d.addCallback(lambda x: fdb.collectData())
+        d.addCallback(lambda x: arp.collectData())
+        return d
+
 summit = ExtremeSummit()
+blackdiamond = ExtremeWare()
--- wiremaps/collector/fdb.py
+++ wiremaps/collector/fdb.py
@@ -56,3 +56,63 @@ class FdbCollector:
                                                                self.fdb,
                                                                self.proxy.ip))
         return d
+
+class ExtremeFdbCollector(FdbCollector):
+
+    # It is really EXTREME-FDB-MIB::extremeFdbMacFdbMacAddress
+    dot1dTpFdbPort = '.1.3.6.1.4.1.1916.1.16.1.1.3'
+    vlanOid = '.1.3.6.1.4.1.1916.1.2.6.1.1'
+    vlanTagged = '.1.3.6.1.4.1.1916.1.2.6.1.1.1'
+    vlanUntagged = '.1.3.6.1.4.1.1916.1.2.6.1.1.2'
+
+    def __init__(self, *args, **kwargs):
+        FdbCollector.__init__(self, *args, **kwargs)
+        self.vlans = None
+
+    def gotVlans(self, results, fdbresults):
+        """Process VLAN results.
+
+        We assume that n/m has index n*1000 + m
+        """
+        if self.vlans is None:
+            self.vlans = {}
+        for oid in results:
+            slot = int(oid.split(".")[-1])
+            vlan = int(oid.split(".")[-2])
+            ports = results[oid]
+            l = self.vlans.get(vlan, [])
+            for i in range(0, len(ports)):
+                if ord(ports[i]) == 0:
+                    continue
+                for j in range(0, 8):
+                    if ord(ports[i]) & (1 << j):
+                        l.append(8-j + 8*i + 1000*slot)
+            self.vlans[vlan] = l
+        self.gotFdb(fdbresults)
+
+    def gotFdb(self, results):
+        """Callback handling reception of FDB
+
+        @param results: result of walking C{EXTREME-BASE-MIB::extremeFdb}
+        """
+        if self.vlans is None:
+            d = self.proxy.walk(self.vlanOid, results)
+            d.addCallback(self.gotVlans, results)
+            return d
+        for oid in results:
+            vlan = int(oid.split(".")[-2])
+            mac = results[oid]
+            mac = ":".join([("%02x" % ord(m)) for m in mac])
+            if mac in ['ff:ff:ff:ff:ff:ff', # Broadcast
+                       '01:80:c2:00:00:0e', # LLDP
+                       '01:80:c2:00:00:02', # Something like LLDP
+                       '00:e0:2b:00:00:02', # Something Extreme
+                       '00:e0:2b:00:00:00', # Again, Extreme
+                       '00:01:30:18:5d:30', # Dunno
+                       '01:01:30:18:5d:30' ]: continue
+            # Rather bad assumption: a vlan is a set of ports
+            for port in self.vlans.get(vlan, []):
+                if self.normport is not None:
+                    port = self.normport(port)
+                if port is not None:
+                    self.fdb[(port, mac)] = 1
