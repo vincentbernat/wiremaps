0f22d078d92418f98427927848bd42157142046e
Vincent Bernat <vbernat@wanadooportails.com>
2008-08-20 16:53:27 +0200

    Fix FDB problem with BlackDiamond running XOS

--- wiremaps/collector/extreme.py
+++ wiremaps/collector/extreme.py
@@ -9,20 +9,22 @@ from wiremaps.collector.arp import ArpCollector
 from wiremaps.collector.lldp import LldpCollector
 
 class ExtremeSummit:
-    """Collector for Extreme Summit and BlackDiamond 8810 switches"""
+    """Collector for Extreme Summit"""
 
     implements(ICollector, IPlugin)
 
     def handleEquipment(self, oid):
         return (oid in ['.1.3.6.1.4.1.1916.2.40', # Extreme Summit 24e
                         '.1.3.6.1.4.1.1916.2.54', # Extreme Summit 48e
-                        '.1.3.6.1.4.1.1916.2.62', # Black Diamond 8810
                         '.1.3.6.1.4.1.1916.2.76', # Extreme Summit 48t
                         ])
 
+    def normPort(self, port):
+        return port
+
     def collectData(self, ip, proxy, dbpool):
         ports = PortCollector(proxy, dbpool)
-        fdb = FdbCollector(proxy, dbpool)
+        fdb = FdbCollector(proxy, dbpool, self.normPort)
         arp = ArpCollector(proxy, dbpool)
         # LLDP disabled due to unstability
         # lldp = LldpCollector(proxy, dbpool)
@@ -33,6 +35,18 @@ class ExtremeSummit:
         d.addCallback(lambda x: fdb.collectData())
         d.addCallback(lambda x: arp.collectData())
         return d
+
+class BlackDiamondXOS(ExtremeSummit):
+    """Collector for BlackDiamond 8810"""
+
+    def handleEquipment(self, oid):
+        return (oid in ['.1.3.6.1.4.1.1916.2.62', # Black Diamond 8810
+                        ])
+
+    def normPort(self, port):
+        slot = port / 128 + 1
+        port = port % 128
+        return slot * 1000 + port
 
 class ExtremeWare:
     """Collector for BlackDiamond 6808"""
@@ -58,4 +72,5 @@ class ExtremeWare:
         return d
 
 summit = ExtremeSummit()
+blackdiamondxos = BlackDiamondXOS()
 blackdiamond = ExtremeWare()
