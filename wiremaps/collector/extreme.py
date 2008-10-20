20e349fb425f988989eb55f197f62bb705e1f21a
Vincent Bernat <vbernat@wanadooportails.com>
2008-10-20 17:56:46 +0200

    Merge all ExtremeWare stuff

--- wiremaps/collector/extreme.py
+++ wiremaps/collector/extreme.py
@@ -18,6 +18,7 @@ class ExtremeSummit:
         return (oid in ['.1.3.6.1.4.1.1916.2.40', # Extreme Summit 24e
                         '.1.3.6.1.4.1.1916.2.54', # Extreme Summit 48e
                         '.1.3.6.1.4.1.1916.2.76', # Extreme Summit 48t
+                        '.1.3.6.1.4.1.1916.2.11', # Black Diamond 6808 (ExtremeWare)
                         ])
 
     def collectData(self, ip, proxy, dbpool):
@@ -48,31 +49,5 @@ class BlackDiamondXOS(ExtremeSummit):
         port = port % 128
         return slot * 1000 + port
 
-class ExtremeWare:
-    """Collector for BlackDiamond 6808"""
-
-    implements(ICollector, IPlugin)
-
-    def handleEquipment(self, oid):
-        return (oid in ['.1.3.6.1.4.1.1916.2.11', # Black Diamond 6808 (ExtremeWare)
-                        ])
-
-    def collectData(self, ip, proxy, dbpool):
-        ports = PortCollector(proxy, dbpool)
-        fdb = ExtremeFdbCollector(proxy, dbpool, self.config)
-        arp = ArpCollector(proxy, dbpool, self.config)
-        edp = EdpCollector(proxy, dbpool)
-        # LLDP disabled due to unstability
-        # lldp = LldpCollector(proxy, dbpool)
-        d = ports.collectData()
-        d.addCallback(lambda x: fdb.collectData(write=False))
-        d.addCallback(lambda x: arp.collectData(write=False))
-        d.addCallback(lambda x: edp.collectData())
-        # d.addCallback(lambda x: lldp.collectData())
-        d.addCallback(lambda x: fdb.collectData())
-        d.addCallback(lambda x: arp.collectData())
-        return d
-
 summit = ExtremeSummit()
 blackdiamondxos = BlackDiamondXOS()
-blackdiamond = ExtremeWare()
