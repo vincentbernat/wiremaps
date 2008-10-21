6c2c15b7d7158ba24d8f26e4ae34390e6f8545dd
Vincent Bernat <bernat@luffy.cx>
2008-10-21 21:11:21 +0200

    For the record, a try to get Cisco VLAN using the generic VlanCollector.
    This does not work because the content of
    vmMembershipSummaryMember2kPorts is not related to ifIndex but uses
    slot/port convention...

--- wiremaps/collector/cisco.py
+++ wiremaps/collector/cisco.py
@@ -1,3 +1,4 @@
+import re
 from zope.interface import implements
 from twisted.plugin import IPlugin
 from twisted.internet import defer
@@ -7,6 +8,7 @@ from wiremaps.collector.port import PortCollector
 from wiremaps.collector.arp import ArpCollector
 from wiremaps.collector.fdb import FdbCollector
 from wiremaps.collector.cdp import CdpCollector
+from wiremaps.collector.vlan import VlanCollector
 
 class Cisco:
     """Collector for Cisco"""
@@ -17,11 +19,11 @@ class Cisco:
         return oid.startswith('.1.3.6.1.4.1.9.')
 
     def collectData(self, ip, proxy, dbpool):
-        ports = PortCollector(proxy, dbpool)
+        self.ports = PortCollector(proxy, dbpool)
         arp = ArpCollector(proxy, dbpool, self.config)
         fdb = FdbCollector(proxy, dbpool, self.config)
         cdp = CdpCollector(proxy, dbpool)
-        d = ports.collectData()
+        d = self.ports.collectData()
         d.addCallback(lambda x: arp.collectData(write=False))
         d.addCallback(lambda x: fdb.collectData(write=False))
         d.addCallback(lambda x: cdp.collectData())
@@ -30,3 +32,7 @@ class Cisco:
         return d
 
 cisco = Cisco()
+
+class CiscoVlanCollector(VlanCollector):
+    oidVlanNames = '.1.3.6.1.4.1.9.9.46.1.3.1.1.4'     # vtpVlanName
+    oidVlanPorts = '.1.3.6.1.4.1.9.9.68.1.2.1.1.3'     # vmMembershipSummaryMember2kPorts
