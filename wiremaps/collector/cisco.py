a84ed57de8c4d934768f9abe9e5f1af736c7c080
Vincent Bernat <bernat@luffy.cx>
2008-10-21 21:24:32 +0200

    Don't try to get VLAN from Cisco, yet.

--- wiremaps/collector/cisco.py
+++ wiremaps/collector/cisco.py
@@ -1,4 +1,3 @@
-import re
 from zope.interface import implements
 from twisted.plugin import IPlugin
 from twisted.internet import defer
@@ -8,7 +7,6 @@ from wiremaps.collector.port import PortCollector
 from wiremaps.collector.arp import ArpCollector
 from wiremaps.collector.fdb import FdbCollector
 from wiremaps.collector.cdp import CdpCollector
-from wiremaps.collector.vlan import VlanCollector
 
 class Cisco:
     """Collector for Cisco"""
@@ -32,7 +30,3 @@ class Cisco:
         return d
 
 cisco = Cisco()
-
-class CiscoVlanCollector(VlanCollector):
-    oidVlanNames = '.1.3.6.1.4.1.9.9.46.1.3.1.1.4'     # vtpVlanName
-    oidVlanPorts = '.1.3.6.1.4.1.9.9.68.1.2.1.1.3'     # vmMembershipSummaryMember2kPorts
