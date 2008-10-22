7ff60791ccd74596f672260f16d65c952c33d5f6
Vincent Bernat <vbernat@wanadooportails.com>
2008-10-22 15:06:59 +0200

    Add VLAN support for Passport

--- wiremaps/collector/passport.py
+++ wiremaps/collector/passport.py
@@ -8,6 +8,7 @@ from wiremaps.collector.fdb import FdbCollector
 from wiremaps.collector.arp import ArpCollector
 from wiremaps.collector.sonmp import SonmpCollector
 from wiremaps.collector.mlt import MltCollector
+from wiremaps.collector.vlan import VlanCollector
 
 class NortelPassport:
     """Collector for ERS8600 Nortel Passport routing switches"""
@@ -41,11 +42,18 @@ class NortelPassport:
         fdb = FdbCollector(proxy, dbpool, self.config, lambda x: self.normPortIndex(x, mlt))
         arp = ArpCollector(proxy, dbpool, self.config)
         sonmp = SonmpCollector(proxy, dbpool, lambda x: x+63)
+        vlan = NortelVlanCollector(proxy, dbpool, lambda x: x-1)
         d = ports.collectData()
         d.addCallback(lambda x: mlt.collectData())
         d.addCallback(lambda x: fdb.collectData())
         d.addCallback(lambda x: arp.collectData())
         d.addCallback(lambda x: sonmp.collectData())
+        d.addCallback(lambda x: vlan.collectData())
         return d
 
 passport = NortelPassport()
+
+class NortelVlanCollector(VlanCollector):
+    """Collect VLAN information for Nortel Passport switchs without LLDP"""
+    oidVlanNames = '.1.3.6.1.4.1.2272.1.3.2.1.2' # rcVlanName
+    oidVlanPorts = '.1.3.6.1.4.1.2272.1.3.2.1.11' # rcVlanPortMembers
