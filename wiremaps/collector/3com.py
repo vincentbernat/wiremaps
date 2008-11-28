0952858df44e0a988bcb7c114230bce1730d40ef
Vincent Bernat <bernat@luffy.cx>
2008-11-28 21:42:34 +0100

    Add VLAN support for 3Com

--- wiremaps/collector/3com.py
+++ wiremaps/collector/3com.py
@@ -28,9 +28,105 @@ class SuperStack:
         ports = PortCollector(proxy, dbpool, self.normPortName)
         fdb = FdbCollector(proxy, dbpool, self.config)
         arp = ArpCollector(proxy, dbpool, self.config)
+        vlan = SuperStackVlanCollector(proxy, dbpool)
         d = ports.collectData()
         d.addCallback(lambda x: fdb.collectData())
         d.addCallback(lambda x: arp.collectData())
+        d.addCallback(lambda x: vlan.collectData())
         return d
 
 superstack = SuperStack()
+
+class SuperStackVlanCollector:
+    """VLAN collector for 3Com SuperStack.
+
+    Here is how this works:
+     - a3ComVlanIfGlobalIdentifier.{x} = {vid}
+     - a3ComVlanIfDescr.{x} = {description}
+     - a3ComVlanEncapsIfTag.{y} = {vid}
+     - a3ComVlanEncapsIfType.{y} = vlanEncaps8021q(2)
+     - ifStackStatus.{y}.{port} = active(1)
+
+    So, walk a3ComVlanIfGlobalIdentifier to get all possible vid, then
+    search for a match in a3ComVlanEncapsIfTag. You should get several
+    match. You need to choose the one where a3ComVlanEncapsIfTag is
+    equal to 2. Then, get the port using ifStackStatus.
+    """
+
+    ifGlobalIdentifier = '.1.3.6.1.4.1.43.10.1.14.1.2.1.4'
+    ifDescr = '.1.3.6.1.4.1.43.10.1.14.1.2.1.2'
+    encapsIfTag = '.1.3.6.1.4.1.43.10.1.14.4.1.1.3'
+    encapsIfType = '.1.3.6.1.4.1.43.10.1.14.4.1.1.2'
+    ifStackStatus = '.1.3.6.1.2.1.31.1.2.1.3'
+
+    def __init__(self, proxy, dbpool):
+        self.proxy = proxy
+        self.dbpool = dbpool
+
+    def gotVlan(self, results, dic):
+        """Callback handling reception of VLAN
+
+        @param results: vlan names or ports
+        @param dic: where to store the results
+        """
+        for oid in results:
+            vid = int(oid.split(".")[-1])
+            dic[vid] = results[oid]
+
+    def gotStackStatus(self, results):
+        """Handle reception of C{IF-MIB::ifStackStatus}.
+
+        We'll build C{vlanPorts}
+        """
+        self.vlanPorts = {}
+        for oid in results:
+            if results[oid] == 1: # active
+                port = int(oid.split(".")[-1])
+                y = int(oid.split(".")[-2])
+                if y not in self.vlanEncapsType:
+                    continue
+                if self.vlanEncapsType[y] == 2: # vlanEncaps8021q
+                    vid = self.vlanEncapsTag[y]
+                    if vid not in self.vlanPorts:
+                        self.vlanPorts[vid] = []
+                    self.vlanPorts[vid].append(port)
+
+    def collectData(self):
+        """Collect VLAN data from SNMP"""
+
+        def fileVlanIntoDb(txn, vid, names, ports, ip):
+            txn.execute("DELETE FROM vlan WHERE equipment=%(ip)s AND type='local'",
+                        {'ip': str(ip)})
+            for x in vid:
+                if vid[x] in ports:
+                    for port in ports[vid[x]]:
+                        txn.execute("INSERT INTO vlan VALUES (%(ip)s, "
+                                    "%(port)s, %(vid)s, %(name)s, "
+                                    "%(type)s)",
+                                    {'ip': str(ip),
+                                     'port': port,
+                                     'vid': vid[x],
+                                     'name': names[x],
+                                     'type': 'local'})
+
+        print "Collecting VLAN information for %s" % self.proxy.ip
+        self.vlanVid = {}
+        self.vlanNames = {}
+        self.vlanEncapsTag = {}
+        self.vlanEncapsType = {}
+        d = self.proxy.walk(self.ifGlobalIdentifier)
+        d.addCallback(self.gotVlan, self.vlanVid)
+        d.addCallback(lambda x: self.proxy.walk(self.ifDescr))
+        d.addCallback(self.gotVlan, self.vlanNames)
+        d.addCallback(lambda x: self.proxy.walk(self.encapsIfTag))
+        d.addCallback(self.gotVlan, self.vlanEncapsTag)
+        d.addCallback(lambda x: self.proxy.walk(self.encapsIfType))
+        d.addCallback(self.gotVlan, self.vlanEncapsType)
+        d.addCallback(lambda x: self.proxy.walk(self.ifStackStatus))
+        d.addCallback(self.gotStackStatus)
+        d.addCallback(lambda x: self.dbpool.runInteraction(fileVlanIntoDb,
+                                                           self.vlanVid,
+                                                           self.vlanNames,
+                                                           self.vlanPorts,
+                                                           self.proxy.ip))
+        return d
