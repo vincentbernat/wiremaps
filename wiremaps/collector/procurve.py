98229d1b39c0fab35e5d4634ae7c953364288a41
Vincent Bernat <vbernat@wanadooportails.com>
2008-12-01 17:11:55 +0100

    Add support for trunk for HP Procurve

--- wiremaps/collector/procurve.py
+++ wiremaps/collector/procurve.py
@@ -24,7 +24,9 @@ class Procurve:
         return port
 
     def collectData(self, ip, proxy, dbpool):
-        ports = PortCollector(proxy, dbpool)
+        t = {}
+        trunk = ProcurveTrunkCollector(proxy, dbpool, t)
+        ports = PortCollector(proxy, dbpool, trunk=t)
         fdb = FdbCollector(proxy, dbpool, self.config,
                            lambda x: self.normport(x, ports))
         arp = ArpCollector(proxy, dbpool, self.config)
@@ -42,3 +44,55 @@ class Procurve:
         return d
 
 procurve = Procurve()
+
+class ProcurveTrunkCollector:
+    """Collect trunk for HP Procurve Switches.
+
+    A trunk is just an interface of type propMultiplexor(54) and the
+    members are found using ifStackStatus.
+    """
+
+    ifType = '.1.3.6.1.2.1.2.2.1.3'
+    ifStackStatus = '.1.3.6.1.2.1.31.1.2.1.3'
+
+    def __init__(self, proxy, dbpool, trunk):
+        self.proxy = proxy
+        self.dbpool = dbpool
+        self.trunk = trunk
+
+    def gotType(self, results):
+        """Callback handling reception of ifType
+
+        @param results: C{IF-MIB::ifType}
+        """
+        for oid in results:
+            if results[oid] == 54:
+                port = int(oid.split(".")[-1])
+                self.trunk[port] = []
+
+    def gotStatus(self, results):
+        """Callback handling reception of stack members
+
+        @param results: C{IF-MIB::ifStackStatus}
+        """
+        for oid in results:
+            physport = int(oid.split(".")[-1])
+            trunkport = int(oid.split(".")[-2])
+            if trunkport in self.trunk:
+                self.trunk[trunkport].append(physport)
+        empty = []
+        for key in self.trunk:
+            if len(self.trunk[key]) == 0:
+                empty.append(key)
+        for key in empty:
+            del self.trunk[key]
+
+    def collectData(self):
+        """Collect link aggregation information from HP Procurve"""
+        print "Collecting trunk information for %s" % self.proxy.ip
+        d = self.proxy.walk(self.ifType)
+        d.addCallback(self.gotType)
+        d.addCallback(lambda x: self.proxy.walk(self.ifStackStatus))
+        d.addCallback(self.gotStatus)
+        return d
+
