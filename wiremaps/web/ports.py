4f0162278410bbedbce745e51aaa4ebfcaed09d5
Vincent Bernat <bernat@luffy.cx>
2009-06-14 22:38:31 +0200

    Fix LEFT JOIN to use extended ports.
    Some ports were skipped when speed information was not available any
    more.

--- wiremaps/web/ports.py
+++ wiremaps/web/ports.py
@@ -222,9 +222,9 @@ SELECT p.speed, ep.speed, ep.duplex, ep.autoneg
 FROM port p
 LEFT JOIN extendedport ep
 ON p.equipment=ep.equipment AND p.index=ep.index
+AND ep.deleted='infinity'
 WHERE p.equipment=%(ip)s AND p.index=%(port)s
 AND p.deleted='infinity'
-AND (ep.deleted='infinity' or ep.deleted IS NULL)
 """
 
     def render(self, data):
