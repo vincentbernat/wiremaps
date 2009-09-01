d45f433bfb97e95ad89f8638584644c4cf812ee1
Vincent Bernat <bernat@luffy.cx>
2009-09-01 13:52:45 +0200

    Be more generic with Nortel baystack switchs.

--- wiremaps/collector/equipment/5510.py
+++ wiremaps/collector/equipment/5510.py
@@ -12,19 +12,12 @@ from wiremaps.collector.helpers.vlan import VlanCollector
 from wiremaps.collector.helpers.nortel import NortelSpeedCollector
 
 class Nortel5510:
-    """Collector for Nortel 55x0 and Nortel 425"""
+    """Collector for Nortel Baystack-like switchs (55xx, 425, etc.)"""
 
     implements(ICollector, IPlugin)
 
     def handleEquipment(self, oid):
-        return (oid in ['.1.3.6.1.4.1.45.3.52.1', # 5510-24T
-                        '.1.3.6.1.4.1.45.3.53.1', # 5510-48T
-                        '.1.3.6.1.4.1.45.3.59.1', # 5520-24T-PWR
-                        '.1.3.6.1.4.1.45.3.59.2', # 5520-48T-PWR
-                        '.1.3.6.1.4.1.45.3.57.1', # 425-48T
-                        '.1.3.6.1.4.1.45.3.57.2', # 425-24T
-                        '.1.3.6.1.4.1.45.3.65', # 5530-24TFD
-                        ])
+        return oid.startswith('.1.3.6.1.4.1.45.3.')
 
     def normPortName(self, port):
         try:
