3407b167829d0750365e3b18edfc3748d5fe6b11
Vincent Bernat <bernat@luffy.cx>
2010-03-24 16:58:20 +0100

    Provide a compatibility layer with the "old" web service API

--- wiremaps/web/site.py
+++ wiremaps/web/site.py
@@ -1,9 +1,10 @@
 import os
 
 from twisted.python import util
+from zope.interface import implements
 from nevow import rend, appserver, loaders, page
 from nevow import tags as T
-from nevow import static
+from nevow import static, inevow
 
 from wiremaps.web.api import ApiResource
 
@@ -30,3 +31,26 @@ class MainPage(rend.Page):
 
     def child_api(self, ctx):
         return ApiResource(self.config, self.dbpool, self.collector)
+
+    def childFactory(self, ctx, node):
+        """Backward compatibility with previous API"""
+        if node in ["equipment", "search", "complete", "past", "images"]:
+            inevow.IRequest(ctx).rememberRootURL()
+            return RedirectApi()
+        return None
+
+class RedirectApi(object):
+    """Redirect to new API.
+
+    rememberRootURL() should be done at root!
+    """
+    implements(inevow.IResource)
+
+    def locateChild(self, ctx, segments):
+        return self, ()
+
+    def renderHTTP(self, ctx):
+        request = inevow.IRequest(ctx)
+        request.redirect("%sapi/1.0%s" % (request.getRootURL(), request.uri))
+        request.setResponseCode(301)
+        return ''
