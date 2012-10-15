754b776a41efe2279d8d826a1b0f3f74c8c85cf9
Vincent Bernat <bernat@luffy.cx>
2012-10-15 20:40:01 +0200

    web: use HTTP redirect for images

--- wiremaps/web/images.py
+++ wiremaps/web/images.py
@@ -5,7 +5,8 @@ from twisted.python import util
 from twisted.enterprise import adbapi
 from twisted.internet import defer
 
-from nevow import rend, appserver, static
+from nevow import rend, appserver, static, inevow
+from nevow.url import URLRedirectAdapter
 from IPy import IP
 
 class ImageResource(rend.Page):
@@ -26,7 +27,7 @@ class ImageResource(rend.Page):
             return appserver.NotFound
         oid = segments[0]
 
-        # Is it really an OID?
+        # Is it an IP?
         try:
             ip = IP(oid)
             d = self.dbpool.runQueryInPast(ctx,
@@ -34,7 +35,7 @@ class ImageResource(rend.Page):
                                      "WHERE ip=%(ip)s AND deleted='infinity'",
                                      {'ip': str(ip)})
         except ValueError:
-            # Well, this is not an IP
+            # Is it an hostname or an OID?
             if not re.match(r"[0-9\.]+", oid):
                 # This should be an hostname
                 d = self.dbpool.runQueryInPast(ctx,
@@ -44,24 +45,29 @@ class ImageResource(rend.Page):
                                          "OR name ILIKE %(name)s||'.%%')",
                                          {'name': oid})
             else:
-                d = defer.succeed([[oid]])
-        d.addCallback(self.getOid)
+                # It's an OID!
+                if oid.startswith("."):
+                    oid = oid[1:]
+                if oid.endswith("."):
+                    oid = oid[:-1]
+                target = os.path.join(self.image_dir, "%s.png" % oid)
+                if os.path.exists(target):
+                    return static.File(target), ()
+                return static.File(os.path.join(self.image_dir, "unknown.png")), ()
+        d.addCallback(self.getOid, ctx)
         return d
 
-    def getOid(self, oid):
-        """Return the static file containing the image corresponding to OID.
-        
+    def getOid(self, oid, ctx):
+        """
+        Return a redirect to the appropriate file
+
         @param oid: OID to use to locate image (can be C{[[oid],...]})
         @return: C{static.File} of the corresponding image
         """
         if oid:
-            if type([]) == list:
+            if type(oid) == list:
                 oid = oid[0][0]
-            if oid.startswith("."):
-                oid = oid[1:]
-            if oid.endswith("."):
-                oid = oid[:-1]
-            target = os.path.join(self.image_dir, "%s.png" % oid)
-        if not oid or not os.path.exists(target):
-            return static.File(os.path.join(self.image_dir, "unknown.png")), ()
-        return static.File(target), ()
+            request = inevow.IRequest(ctx)
+            print request.URLPath().child(oid)
+            return URLRedirectAdapter(request.URLPath().child(oid)), ()
+        return appserver.NotFound
