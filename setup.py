ffc5dd55c2efb39d967afd384f7135229c38ed74
Vincent Bernat <vincent@bernat.im>
2015-03-05 10:08:01 +0100

    setup: fix setup.py to be PEP8-compliant

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
-import sys
-import twisted
 from distutils.core import setup, Extension
 from wiremaps import VERSION
+
 
 def refresh_plugin_cache():
     from twisted.plugin import IPlugin, getPlugins
@@ -10,26 +9,26 @@ def refresh_plugin_cache():
 if __name__ == "__main__":
     setup(name="wiremaps",
           version=VERSION,
-          classifiers = [
-            'Development Status :: 4 - Beta',
-            'Environment :: No Input/Output (Daemon)',
-            'Environment :: Web Environment',
-            'Framework :: Twisted',
-            'Intended Audience :: System Administrators',
-            'License :: OSI Approved :: GNU General Public License (GPL)',
-            'Operating System :: POSIX',
-            'Programming Language :: Python',
-            'Topic :: System :: Networking',
-            ],
+          classifiers=[
+              'Development Status :: 4 - Beta',
+              'Environment :: No Input/Output (Daemon)',
+              'Environment :: Web Environment',
+              'Framework :: Twisted',
+              'Intended Audience :: System Administrators',
+              'License :: OSI Approved :: GNU General Public License (GPL)',
+              'Operating System :: POSIX',
+              'Programming Language :: Python',
+              'Topic :: System :: Networking',
+          ],
           url='https://github.com/vincentbernat/wiremaps',
           description='layer 2 network discovery application',
           author='Vincent Bernat',
           author_email="bernat@luffy.cx",
-          ext_modules= [
-            Extension('wiremaps.collector.snmp',
-                      libraries = ['netsnmp', 'crypto'],
-                      sources= ['wiremaps/collector/snmp.c']),
-            ],
+          ext_modules=[
+              Extension('wiremaps.collector.snmp',
+                        libraries=['netsnmp', 'crypto'],
+                        sources=['wiremaps/collector/snmp.c']),
+          ],
           packages=["wiremaps",
                     "wiremaps.collector",
                     "wiremaps.collector.equipment",
@@ -41,6 +40,5 @@ if __name__ == "__main__":
                         'wiremaps.web': ["static/*.png", "static/*.gif",
                                          "static/*.css", "static/*.js",
                                          "main.xhtml",
-                                         "images/1.*.png", "images/unknown.png"],},
-          )
-    # refresh_plugin_cache()
+                                         "images/1.*.png",
+                                         "images/unknown.png"]})
